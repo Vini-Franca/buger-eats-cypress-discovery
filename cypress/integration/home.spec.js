51479b235c213dd06ab5f667191ebe3bae49d587
Vini-Franca <vinicius_santana_rf@hotmail.com>
2022-04-19 21:17:02 -0300

    home spec

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,6 @@
 describe("home page", () => {
   it("app deve estar online", () => {
-    cy.viewport(1440, 900);
-    cy.visit("https://buger-eats.vercel.app/");
+    cy.visit("/");
     cy.get(".signup").should("have.text", "Ou se cadastre  ");
   });
 });
